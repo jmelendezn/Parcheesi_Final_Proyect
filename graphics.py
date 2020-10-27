c4e95c91b47bdcb17ada656dbfbc8e2c527a3e1e
Josue Melendez <melendez_103733@students.pupr.edu>
2020-10-27 17:54:35 -0400

    Continuacion
    Se continuo trabajando con el desarrollo del juego Parcheesi

--- graphics.py
+++ graphics.py
@@ -164,7 +164,8 @@ import time, os, sys
 
 try:  # import as appropriate for 2.x vs. 3.x
     import tkinter as tk
-
+except:
+    import Tkinter as tk
 
 
 ##########################################################################
